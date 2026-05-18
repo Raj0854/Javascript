cd1855628d0b3bfd2c1c239889320135c482b094
Raj gupta <rajgupta.02.0854@gmail.com>
2026-05-18 16:19:28 +0530

    Create practiced questions.js

---
+++ 01 Variables Data types/practiced questions.js
@@ -0,0 +1,23 @@
+// Q.1 Create a const object product with properties name, price,offer, rating and category
+const product = {
+    productName : "Lenovo Laptop",
+    price : 65000.00,
+    offer : 10 + "%",
+    rating : 4.5,
+    category : "Electronics",
+
+}
+
+console.log(product);
+
+
+// Q.2 Create a const object profile with properties username, isFollowing, followers, following and posts
+const profile = {
+    username : "akash_kumar",
+    isFollowing : true,
+    followers : 1000,
+    following : 500,
+    posts : 50
+};
+
+console.log(profile);
