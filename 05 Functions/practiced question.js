08150e8ed4fb1d4305c27af56c3bb7ae597a958e
Raj gupta <rajgupta.02.0854@gmail.com>
2026-05-26 13:52:05 +0530

    Update practiced question.js

--- 05 Functions/practiced question.js
+++ 05 Functions/practiced question.js
@@ -26,7 +26,7 @@ nums.forEach((val)=>{console.log(val*val)
 
 
 // Q.3 // we are given array marks of student filter out of student that is scored 90 +
-let marks =[1,2,3,4,5,6,7,8,9]
+let marks =[78, 85, 92, 88, 95, 80, 91, 89]
 let toppers= marks.filter((val)=>{return val>90})
 
 
@@ -34,3 +34,5 @@ let toppers= marks.filter((val)=>{return val>90})
 
 
 //Q.4 Take a number  as n input from user create an array of number from one to N use the reduce matter to calculate some of the all numbers in the area and use reduce matter to calculate product of all numbers in the array
+let n= parseInt(prompt("Enter a number:"));
+let arr= Array.from({length:n}, (_,i)=>i+1);
