450e6028c31891ecb5e901d795e56a4e90f526ec
Raj gupta <rajgupta.02.0854@gmail.com>
2026-05-26 13:52:12 +0530

    Update arrow functions.js

---
+++ 05 Functions/arrow functions.js
@@ -0,0 +1,17 @@
+// arrow function
+// arrow functions are a shorter syntax for writing function expressions
+// they are anonymous functions that can be assigned to a variable
+const functionName = () => { // code to be executed 
+    };
+// arrow function with parameters
+const functionName = (parameter1, parameter2) => {
+    // code to be executed
+};
+// arrow function with return value
+const functionName = (parameter1, parameter2) => {
+    // code to be executed
+    return parameter1 + parameter2;
+};
+// arrow function with implicit return value
+// if the function body contains only one expression, we can omit the curly braces and the return keyword
+const functionName = (parameter1, parameter2) => parameter1 + parameter2;
