314e4604bc861b25c79751604dae8db13ef7146d
Raj gupta <rajgupta.02.0854@gmail.com>
2026-05-28 16:23:00 +0530

    Create practiced questions.js

---
+++ 07 Events/practiced questions.js
@@ -0,0 +1,16 @@
+//  craete a button and add event listener to it for dark mode toggle when clicked light mode and dark mode should be toggled on each click
+ let button = document.querySelector('button');
+let body = document.querySelector('body');
+let mode = 'light';
+
+button.addEventListener('click', function() {
+    if (mode === 'light') {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+        mode = 'dark';
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+        mode = 'light';
+    }
+});
