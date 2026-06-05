c3d027a27c993514668142309e7c329cd893fa74
Raj gupta <rajgupta.02.0854@gmail.com>
2026-06-05 16:53:28 +0530

    Create inheritance.js

---
+++ 08 Classes and objects/inheritance.js
@@ -0,0 +1,26 @@
+// inheritance is a fundamental concept in object-oriented programming that allows a new class (called a subclass or child class) to inherit properties and behaviors (methods) from an existing class (called a superclass or parent class). This promotes code reusability and establishes a natural hierarchical relationship between classes.
+
+// example 01   
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    eat() {
+        console.log(`${this.name} is eating.`);
+    }
+}
+
+// Dog class inherits from Animal class
+class Dog extends Animal {
+    constructor( breed) {
+        this.breed = breed;
+    }
+    bark() {
+        console.log(`${this.name} is barking.`);
+    }
+}
+
+const dog1 = new Dog('Labrador');
+dog1.eat(); // Output: Labrador is eating.
+dog1.bark(); // Output: Labrador is barking.
+// In this example, the Dog class extends the Animal class, which means it inherits the properties and methods of the Animal class. The Dog class can use the eat method defined in the Animal class, and it also has its own method bark.
