8d235095e9d5fc833e1c43bd088857109f25e9d2
Raj gupta <rajgupta.02.0854@gmail.com>
2026-06-05 16:54:43 +0530

    Update inheritance.js

--- 08 Classes and objects/inheritance.js
+++ 08 Classes and objects/inheritance.js
@@ -24,3 +24,5 @@ const dog1 = new Dog('Labrador');
 dog1.eat(); // Output: Labrador is eating.
 dog1.bark(); // Output: Labrador is barking.
 // In this example, the Dog class extends the Animal class, which means it inherits the properties and methods of the Animal class. The Dog class can use the eat method defined in the Animal class, and it also has its own method bark.
+
+// note: if child  and parent class have same method then child class method will override the parent class method. This is called method overriding. 
