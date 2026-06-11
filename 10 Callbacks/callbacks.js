e2bdaedd9e177ba9bcc37bc6a90ac11115d123ab
Raj gupta <rajgupta.02.0854@gmail.com>
2026-06-11 21:42:40 +0530

    Update callbacks.js

--- 10 Callbacks/callbacks.js
+++ 10 Callbacks/callbacks.js
@@ -1,8 +1,31 @@
-// Callbacks are functions used as arguments into another functions.
-const sum = (a,b)=> {
-    return a+b
-}
-const printSum=(a,b,sumFunction)=>{
-    console.log(sumFunction(a,b))
-}
-printSum(2,3,sum) 
+// // Callbacks are functions used as arguments into another functions.
+// const sum = (a,b)=> {
+//     return a+b
+// }
+// const printSum=(a,b,sumFunction)=>{
+//     console.log(sumFunction(a,b))
+// }
+// printSum(2,3,sum) 
+
+
+function getData(dataID, getNextData) {
+    console.log("getting data of", dataID)
+
+    setTimeout(() => {
+        console.log("data", dataID);
+        if (getNextData) {
+            getNextData();
+        }
+    }, 5000)//2s
+};
+
+// call back hell
+
+getData(1, () => {
+    getData(2, () => {
+        getData(3, () => {
+            getData(4)
+        });
+    });
+});
+
