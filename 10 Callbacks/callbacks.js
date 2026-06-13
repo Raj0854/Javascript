a259ef6449124fec8c95e5d7c6c01b28e6bc4798
Raj gupta <rajgupta.02.0854@gmail.com>
2026-06-13 16:43:01 +0530

    Update callbacks.js

--- 10 Callbacks/callbacks.js
+++ 10 Callbacks/callbacks.js
@@ -1,13 +1,22 @@
-// // Callbacks are functions used as arguments into another functions.
-// const sum = (a,b)=> {
-//     return a+b
-// }
-// const printSum=(a,b,sumFunction)=>{
-//     console.log(sumFunction(a,b))
-// }
-// printSum(2,3,sum) 
+// Callbacks are functions used as arguments into another functions.
+const sum = (a,b)=> {
+    return a+b
+}
+const sub=(a,b)=>{
+    return a-b
+}
+const divide =(a,b)=>{
+    return a/b
+}
+const mutliply=(a,b)=>{
+    return a*b
+}
+const printSum=(a,b,operators)=>{
+    console.log(operators(a,b))
+}
+printSum(8,8,mutliply) 
 
-
+//callback with time out 
 function getData(dataID, getNextData) {
     console.log("getting data of", dataID)
 
@@ -19,7 +28,7 @@ function getData(dataID, getNextData) {
     }, 5000)//2s
 };
 
-// call back hell
+// call back hell is nested callbacks stacked
 
 getData(1, () => {
     getData(2, () => {
