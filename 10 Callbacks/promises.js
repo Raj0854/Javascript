84ea3b8075b481ebd84d057369374a799649bf25
Raj gupta <rajgupta.02.0854@gmail.com>
2026-06-13 17:26:21 +0530

    Update promises.js

--- 10 Callbacks/promises.js
+++ 10 Callbacks/promises.js
@@ -3,15 +3,47 @@
 // Resolve and reject are the callback provided by JS.
 // formation:
 // let promise = new Promise((resolve, reject)=>{...})
-let num=parseInt(prompt(("Enter a num")))
-let  promise = new Promise((resolve, reject)=>{
-    if (num===5){
-        console.log("i am promise")
-        resolve("success");}
-    else
-        { reject("error")}
-    
-});
+// let num=parseInt(prompt(("Enter a num")))
+// let  promise = new Promise((resolve, reject)=>{
+//     if (num===5){
+//         console.log("i am promise")
+//         resolve("success");}
+//     else
+//         { reject("error")}
 
-promise.then(()=>{console.log("resoved")});
-promise.catch(()=>{console.log("rejected")});
+// });
+
+// promise.then(()=>{console.log("resoved")});
+// promise.catch(()=>{console.log("rejected")});
+
+
+//Promise Chaining
+// nested promise is chain type structure known as promise chaining
+function getData(dataID) {
+    return new Promise((resolve, reject) => {
+        console.log("getting data of", dataID)
+
+        setTimeout(() => {
+            console.log("data", dataID)
+            resolve("success")
+        }, 2000)
+    })
+};
+// 1. First way of chaining
+getData(5).then((res) => {
+    console.log(res);
+    getData(10).then(() => {
+        console.log(res);
+        getData(25).then(() => {
+            console.log("end")
+        })
+    });
+});
+// 2. Second way of chaining
+getData(8).then(() => {
+    return getData(10)
+}).then(() => {
+    return getData(59)
+}).then(() => {
+    console.log("ended")
+})
