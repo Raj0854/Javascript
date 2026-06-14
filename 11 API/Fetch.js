039621b54f263481171ea6b1be37138a2ead310d
Raj gupta <rajgupta.02.0854@gmail.com>
2026-06-14 21:30:09 +0530

    Update Fetch.js

--- 11 API/Fetch.js
+++ 11 API/Fetch.js
@@ -1,11 +1,33 @@
 // Fetch api provides an interface for fetching(sending/receiving) resoures.
 // API stands for Application Programming interface
-// It uses Request and Repsonse objects. These objects are predefined.
+// It uses Request and Response objects. These objects are predefined.
 // The fetch() method is used to fetch a resource(data) from url.
 // it can be as
 // let promise = fetch(URL,[options])
+// GET is default method in fetch
 const url = "https://jsonplaceholder.typicode.com/users/1"
+
 const getUser = async () => {
-    let User = fetch(url)
-    console.log(User)
+    let response = await fetch(url)//response
+    console.log(response.status)
 }
+// JSON() methods returns a second promise that converts json data into js object which readable.
+
+const getUser = async () => {
+    let response = await fetch(url)//response
+    let data = await response.json()
+    console.log(data)
+
+}
+
+// html connections
+const button = document.querySelector("#btn")
+const details = document.querySelector("#details")
+
+const getUser = async () => {
+    let response = await fetch(url)//response
+    let data = await response.json()
+    console.log(data)
+    details.innerText = `ID = ${data.id}\nUsername = ${data.username}\nEmail = ${data.email}`
+}
+button.addEventListener("click",getUser)
